882c78de25aa0d6bce2d68164e2df9cc6e359101
Pyry Kaleva <pyry.kaleva@gmail.com>
2022-10-04 18:12:44 +0300

    2-11

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Numbers from './components/Numbers'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-  const [filtered, setFiltered] = useState(persons)
+  const [persons, setPersons] = useState([]) 
+  const [filtered, setFiltered] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setFiltered(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
